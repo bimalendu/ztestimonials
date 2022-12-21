1d5a7218462c1f9d10cb2a190a89da2faae940c7
Bimalendu Behera <bimalendu.behera@gmail.com>
2022-12-21 16:55:44 +0530

    #1 Updated plugin and widget page

--- ztestimonials.php
+++ ztestimonials.php
@@ -1,41 +1,41 @@
 <?php
 
 /**
-* Plugin Name: MV Testimonials
-* Plugin URI: https://www.wordpress.org/mv-testimonials
-* Description: My plugin's description
+* Plugin Name: ZTestimonials
+* Plugin URI: https://www.wordpress.org/ztestimonials
+* Description: A plugin to show testimonials
 * Version: 1.0
 * Requires at least: 5.6
 * Requires PHP: 7.0
-* Author: Marcelo Vieira
-* Author URI: https://www.codigowp.net
+* Author: Bimalendu Behera
+* Author URI: https://www.forkics.com
 * License: GPL v2 or later
 * License URI: https://www.gnu.org/licenses/gpl-2.0.html
-* Text Domain: mv-testimonials
+* Text Domain: ztestimonials
 * Domain Path: /languages
 */
 /*
-MV Testimonials is free software: you can redistribute it and/or modify
+ZTestimonials is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
 any later version.
  
-MV Testimonials is distributed in the hope that it will be useful,
+ZTestimonials is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
  
 You should have received a copy of the GNU General Public License
-along with MV Testimonials. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+along with Z Testimonials. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-if( !class_exists( 'MV_Testimonials' ) ){
+if( !class_exists( 'ZTestimonials' ) ){
 
-    class MV_Testimonials{
+    class ZTestimonials{
 
         public function __construct() {
 
@@ -44,11 +44,11 @@ if( !class_exists( 'MV_Testimonials' ) ){
             // Define constants used througout the plugin
             $this->define_constants(); 
             
-            require_once( MV_TESTIMONIALS_PATH . 'post-types/class.mv-testimonials-cpt.php' );
-            $MVTestimonialsPostType = new MV_Testimonials_Post_Type();
+            require_once( ZTESTIMONIALS_PATH . 'post-types/class.ztestimonials-cpt.php' );
+            $ZTestimonialsPostType = new ZTestimonials_Post_Type();
 
-            require_once( MV_TESTIMONIALS_PATH . 'widgets/class.mv-testimonials-widget.php' );
-            $MVTestimonialsWidget = new MV_Testimonials_Widget();   
+            require_once( ZTESTIMONIALS_PATH . 'widgets/class.ztestimonials-widget.php' );
+            $ZTestimonialsWidget = new ZTestimonials_Widget();   
             
             add_filter( 'archive_template', array( $this, 'load_custom_archive_template' ) );
             add_filter( 'single_template', array( $this, 'load_custom_single_template' ) );
@@ -59,42 +59,42 @@ if( !class_exists( 'MV_Testimonials' ) ){
          */
         public function define_constants(){
             // Path/URL to root of this plugin, with trailing slash.
-            define ( 'MV_TESTIMONIALS_PATH', plugin_dir_path( __FILE__ ) );
-            define ( 'MV_TESTIMONIALS_URL', plugin_dir_url( __FILE__ ) );
-            define ( 'MV_TESTIMONIALS_VERSION', '1.0.0' );  
-            define ( 'MV_TESTIMONIALS_OVERRIDE_PATH_DIR', get_stylesheet_directory() . '/mv-testimonials/' );   
+            define ( 'ZTESTIMONIALS_PATH', plugin_dir_path( __FILE__ ) );
+            define ( 'ZTESTIMONIALS_URL', plugin_dir_url( __FILE__ ) );
+            define ( 'ZTESTIMONIALS_VERSION', '1.0.0' );  
+            define ( 'ZTESTIMONIALS_OVERRIDE_PATH_DIR', get_stylesheet_directory() . '/ztestimonials/' );   
         }
 
         public function load_custom_archive_template( $tpl ){
-            if( current_theme_supports( 'mv-testimonials' ) ){
-                if( is_post_type_archive( 'mv-testimonials' ) ){
-                    $tpl = $this->get_template_part_location( 'archive-mv-testimonials.php' );
+            if( current_theme_supports( 'ztestimonials' ) ){
+                if( is_post_type_archive( 'ztestimonials' ) ){
+                    $tpl = $this->get_template_part_location( 'archive-ztestimonials.php' );
                 }
             }
             return $tpl;
         }
 
         public function load_custom_single_template( $tpl ){
-            if( current_theme_supports( 'mv-testimonials' ) ){
-                if( is_singular( 'mv-testimonials' ) ){
-                    $tpl = $this->get_template_part_location( 'single-mv-testimonials.php' );
+            if( current_theme_supports( 'ztestimonials' ) ){
+                if( is_singular( 'ztestimonials' ) ){
+                    $tpl = $this->get_template_part_location( 'single-ztestimonials.php' );
                 }
             }
             return $tpl;
         }
 
         public function get_template_part_location( $file ){
-            if( file_exists( MV_TESTIMONIALS_OVERRIDE_PATH_DIR . $file ) ){
-                $file = MV_TESTIMONIALS_OVERRIDE_PATH_DIR . $file;
+            if( file_exists( ZTESTIMONIALS_OVERRIDE_PATH_DIR . $file ) ){
+                $file = ZTESTIMONIALS_OVERRIDE_PATH_DIR . $file;
             }else{
-                $file = MV_TESTIMONIALS_PATH . 'views/templates/' . $file;
+                $file = ZTESTIMONIALS_PATH . 'views/templates/' . $file;
             }
             return $file;
         }
 
         public function load_textdomain(){
             load_plugin_textdomain(
-                'mv-testimonials',
+                'ztestimonials',
                 false,
                 dirname( plugin_basename( __FILE__ ) ) . '/languages/'
             );
@@ -111,7 +111,7 @@ if( !class_exists( 'MV_Testimonials' ) ){
          * Deactivate the plugin
          */
         public static function deactivate(){
-            unregister_post_type( 'mv-testimonials' );
+            unregister_post_type( 'ztestimonials' );
             flush_rewrite_rules();
         }
 
@@ -120,11 +120,11 @@ if( !class_exists( 'MV_Testimonials' ) ){
          */
         public static function uninstall(){
 
-            delete_option( 'widget_mv-testimonials' );
+            delete_option( 'widget_ztestimonials' );
 
             $posts = get_posts(
                 array(
-                    'post_type' => 'mv-testimonials',
+                    'post_type' => 'ztestimonials',
                     'number_posts'  => -1,
                     'post_status'   => 'any'
                 )
@@ -138,11 +138,11 @@ if( !class_exists( 'MV_Testimonials' ) ){
     }
 }
 
-if( class_exists( 'MV_Testimonials' ) ){
+if( class_exists( 'ZTestimonials' ) ){
     // Installation and uninstallation hooks
-    register_activation_hook( __FILE__, array( 'MV_Testimonials', 'activate'));
-    register_deactivation_hook( __FILE__, array( 'MV_Testimonials', 'deactivate'));
-    register_uninstall_hook( __FILE__, array( 'MV_Testimonials', 'uninstall' ) );
+    register_activation_hook( __FILE__, array( 'ZTestimonials', 'activate'));
+    register_deactivation_hook( __FILE__, array( 'ZTestimonials', 'deactivate'));
+    register_uninstall_hook( __FILE__, array( 'ZTestimonials', 'uninstall' ) );
 
-    $mv_testimonials = new MV_Testimonials();
+    $ztestimonials = new ZTestimonials();
 }
